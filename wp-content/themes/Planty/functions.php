d71cb925323348bdaead75fa5ef1af5b8763fcc7
Audry Perez <auzente01@gmail.com>
2023-12-18 20:42:07 +0100

    premier partie du header, avec le bouton commander avec un fond rose

--- wp-content/themes/Planty/functions.php
+++ wp-content/themes/Planty/functions.php
@@ -1,7 +1,13 @@
 <?php
 
+/* Enqueue des styles du thème enfant */
 add_action('wp_enqueue_scripts', 'theme_enqueue_styles');
 function theme_enqueue_styles()
+
 {
-    wp_enqueue_styles('parent-style', get_template_directory_uri() . '/style.css');
+    /* Enqueue du style du thème parent */
+    wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
+
+    /* Enqueue du style du thème enfant */
+    wp_enqueue_style('child-style', get_stylesheet_uri(), array('parent-style'), wp_get_theme()->get('Version'));
 }
